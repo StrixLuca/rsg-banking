f0df05a9006539d68de6d96f0ff006419ba75ba3
StrixLuca <110634650+StrixLuca@users.noreply.github.com>
2024-04-04 12:44:04 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -3,31 +3,7 @@ local RSGCore = exports['rsg-core']:GetCoreObject()
 -----------------------------------------------------------------------
 -- version checker
 -----------------------------------------------------------------------
-local function versionCheckPrint(_type, log)
-    local color = _type == 'success' and '^2' or '^1'
-
-    print(('^5['..GetCurrentResourceName()..']%s %s^7'):format(color, log))
-end
-
-local function CheckVersion()
-    PerformHttpRequest('https://raw.githubusercontent.com/Rexshack-RedM/rsg-banking/main/version.txt', function(err, text, headers)
-        local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
-
-        if not text then 
-            versionCheckPrint('error', 'Currently unable to run a version check.')
-            return 
-        end
-
-        --versionCheckPrint('success', ('Current Version: %s'):format(currentVersion))
-        --versionCheckPrint('success', ('Latest Version: %s'):format(text))
-        
-        if text == currentVersion then
-            versionCheckPrint('success', 'You are running the latest version.')
-        else
-            versionCheckPrint('error', ('You are currently running an outdated version, please update to version %s'):format(text))
-        end
-    end)
-end
+lib.versionCheck('Rexshack-RedM/rsg-banking')
 
 -- callback for bank balance
 RSGCore.Functions.CreateCallback('rsg-banking:getBankingInformation', function(source, cb)
@@ -163,9 +139,5 @@ RSGCore.Commands.Add('bloodmoneyclip', 'Make Blood Money Clip', {{ name = 'amoun
             lib.notify({ title = 'Blood Money Clip Converted', description = 'You\'ve just converted $'..args1..' bloodmoney into a Money Clip!', type = 'success' })
         end
     end
-end, 'user')
-
---------------------------------------------------------------------------------------------------
--- start version check
---------------------------------------------------------------------------------------------------
-CheckVersion()
+end, 'user'
+    
